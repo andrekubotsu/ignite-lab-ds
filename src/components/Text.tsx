e55727532e06889fa7350bc2c8810cb3e6051e97
Andre Kubotsu <andre.kubotsu@localiza.com>
2022-10-13 10:21:40 -0300

    feat: first component and storybook

---
+++ src/components/Text.tsx
@@ -0,0 +1,25 @@
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
+import { ReactNode } from "react";
+
+export interface TextProps {
+  size?: "sm" | "md" | "lg";
+  children: ReactNode;
+  asChild?: boolean;
+}
+
+export function Text({ size = "md", children, asChild }: TextProps) {
+  const Component = asChild ? Slot : "span";
+
+  return (
+    <Component
+      className={clsx("text-gray-100 font-sans", {
+        "text-xs": size === "sm",
+        "text-sm": size === "md",
+        "text-md": size === "lg",
+      })}
+    >
+      {children}
+    </Component>
+  );
+}
