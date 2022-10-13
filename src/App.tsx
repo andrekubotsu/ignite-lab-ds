a6ecc49d8b651ea14ca7876259304d7f7f9a0ef0
Andre Kubotsu <andre.kubotsu@localiza.com>
2022-10-13 08:48:00 -0300

    chore: initial configs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500">Hello World!</h1>
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  );
+}
