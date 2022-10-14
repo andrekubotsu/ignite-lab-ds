f3030e5cb43ffa473e551c11134ecfce7224a89a
Andre Kubotsu <contato201@gmail.com>
2022-10-14 08:53:23 -0300

    chore: configuring msw

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,11 @@
 import { themes } from "@storybook/theming";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
 import "../src/styles/global.css";
+
+initialize();
+
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
